629a9a4d3b9f30d591015f1f33c3c718340028cc
krishnadev v <krishnadevv12@gmail.comgit config --global user.name krishnadev7>
2021-10-12 15:49:27 +0530

    cat generator second challenge completed

--- static/js/script.js
+++ static/js/script.js
@@ -13,3 +13,10 @@ function ageInDays() {
 function reset() {
   document.getElementById('ageInDays').remove();
 }
+// challenge 2 Cat-Generator
+function generateCat(){
+  var image=document.createElement('img');
+  var div=document.getElementById('flex-cat-gen');
+  image.src='https://thecatapi.com/api/images/get?format=src&type=gif&size=small';
+  div.appendChild(image);
+}
