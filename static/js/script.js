051a06da96db06e64866c704fcb3dd0a81984763
krishnadev v <krishnadevv12@gmail.comgit config --global user.name krishnadev7>
2021-10-12 21:20:28 +0530

    3rd challenge rps finished upto choosing rock,paper,scissors

--- static/js/script.js
+++ static/js/script.js
@@ -1,22 +1,36 @@
 // challenge 1 Age In Days
 function ageInDays() {
-  var birthyear = prompt('What year you were born...???');
+  var birthyear = prompt("What year you were born...???");
   var result = (2021 - birthyear) * 365;
-  var h1 = document.createElement('h1');
-  var textAnswer = document.createTextNode('you are ' + result + '  days old');
-  h1.setAttribute('id', 'ageInDays');
+  var h1 = document.createElement("h1");
+  var textAnswer = document.createTextNode("you are " + result + "  days old");
+  h1.setAttribute("id", "ageInDays");
   h1.appendChild(textAnswer);
-  document.getElementById('flex-box-result').appendChild(h1);
+  document.getElementById("flex-box-result").appendChild(h1);
   console.log(result);
 }
 
 function reset() {
-  document.getElementById('ageInDays').remove();
+  document.getElementById("ageInDays").remove();
 }
 // challenge 2 Cat-Generator
-function generateCat(){
-  var image=document.createElement('img');
-  var div=document.getElementById('flex-cat-gen');
-  image.src='https://thecatapi.com/api/images/get?format=src&type=gif&size=small';
+function generateCat() {
+  var image = document.createElement("img");
+  var div = document.getElementById("flex-cat-gen");
+  image.src =
+    "https://thecatapi.com/api/images/get?format=src&type=gif&size=small";
   div.appendChild(image);
+}
+// challenge 3 Rock,Paper,Scissors
+function rpsGame(yourChoice) {
+  console.log(yourChoice);
+  var humanChoice, botChoice;
+  botChoice = numberToChoice(randToRpsIns());
+  console.log(botChoice);
+}
+function randToRpsIns() {
+  return Math.floor(Math.random() * 3);
+}
+function numberToChoice(number) {
+  return ["rock", "paper", "scissors"][number];
 }
